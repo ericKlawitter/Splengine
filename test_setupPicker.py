538d0d53a3cfc7853d8cf9a51b4bf0b03aa8e22b
Eric Klawitter <5823687+ericKlawitter@users.noreply.github.com>
2019-04-02 20:23:01 -0700

    Use setup method in unit tests

--- test_setupPicker.py
+++ test_setupPicker.py
@@ -11,44 +11,41 @@ nobles = [Noble({white: 4, blue: 4}), Noble({blue: 4, green: 4}), Noble({green: 
 
 class TestSetupPicker(TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.picker = SetupPicker(nobles, 3)
+
     @mock.patch("builtins.input")
     def test_valid_input(self, mock_input):
         mock_input.side_effect = [7, 5, 0]
-        picker = SetupPicker(nobles, 3)
-        self.assertEqual([nobles[7], nobles[5], nobles[0]], picker.pick())
+        self.assertEqual([nobles[7], nobles[5], nobles[0]], self.picker.pick())
 
     @mock.patch("builtins.input", return_value="asdf")
     def test_nonnumeric_input(self, input):
-        picker = SetupPicker(nobles, 3)
         with self.assertRaises(ValueError):
-            picker._get_valid_input(0, set())
+            self.picker._get_valid_input(0, set())
 
     @mock.patch("builtins.input")
     def test_duplicate_input(self, input):
         picked = {4, 1}
-        picker = SetupPicker(nobles, 3)
         with self.assertRaises(ValueError):
-            picker._get_valid_input(4, picked)
+            self.picker._get_valid_input(4, picked)
 
     @mock.patch("builtins.input", return_value=-3)
     def test_less_than_0_input(self, input):
-        picker = SetupPicker(nobles, 1)
         with self.assertRaises(ValueError):
-            picker._get_valid_input(1, set())
+            self.picker._get_valid_input(1, set())
 
     @mock.patch("builtins.input", return_value=51)
     def test_greater_than_len(self, input):
-        picker = SetupPicker(nobles, 1)
         with self.assertRaises(ValueError):
-            picker._get_valid_input(1, set())
+            self.picker._get_valid_input(1, set())
 
     @mock.patch("builtins.input", return_value=3.7)
     def test_decimal_input(self, input):
-        picker = SetupPicker(nobles, 1)
-        self.assertEqual(3, picker._get_valid_input(1, set()))
+        self.assertEqual(3, self.picker._get_valid_input(1, set()))
 
     @mock.patch("builtins.input")
     def test_some_invalid_input(self, inp):
         inp.side_effect = [1, 3, "asdf", 789, 8]
-        picker = SetupPicker(nobles, 3)
-        self.assertEqual([nobles[1], nobles[3], nobles[8]], picker.pick())
+        self.assertEqual([nobles[1], nobles[3], nobles[8]], self.picker.pick())
